56fab85efdc78800acb9094da40bc6ad8321f064
nanareyes <dianac.reyesmantilla@gmail.com>
2021-11-09 18:07:50 -0500

    Se modifican los nombres del schema user y para trabajarlos en los dos front react y vue con terminos similares

--- models/user.js
+++ models/user.js
@@ -2,25 +2,26 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    identificacion: {type:Number, required:[true, 'Identificación requerida']},
-    nombres: {type: String, required: [true, 'Nombre obligatorio']},
-    apellidos: {type: String, required: [true, 'Apellidos obligatorio']}, 
-    fechaNacimiento:{type: Date},
-    direccion: {type: String, required: [true, 'Dirección requerida']},
-    ciudad: {type: String, required: [true, 'Ciudad obligatorio']}, 
-    departamento: {type: String, required: [true, 'Departamento obligatorio']}, 
-    telefono: {type:Number, required:[true, 'teléfono requerido']},
-    email: {type: String, required: [true, 'Email obligatorio']},
-    password: {type: String, required: [true, 'Contraseña obligatoria']},
-    tipo: {type: String, required: [true, 'Tipo de usuario obligatorio']},    
-    terminos: {type: Boolean, required: [true, 'terminos obligatorios']},
+  identification: { type: Number, required: [true, 'Identificación requerida'] },
+  name: { type: String, required: [true, 'Nombre obligatorio'] },
+  lastname: { type: String, required: [true, 'Apellidos obligatorio'] },
+  dateOfBirth: { type: Date },
+  address: { type: String, required: [true, 'Dirección requerida'] },
+  city: { type: String, required: [true, 'Ciudad obligatorio'] },
+  departament: { type: String, required: [true, 'Departamento obligatorio'] },
+  phone: { type: Number, required: [true, 'teléfono requerido'] },
+  email: { type: String, required: [true, 'Email obligatorio'] },
+  password: { type: String, required: [true, 'Contraseña obligatoria'] },
+  type: { type: String, required: [true, 'Tipo de usuario obligatorio'] },
+  terms: { type: Boolean, required: [true, 'terminos obligatorios'] },
 });
 
 // Eliminar pass de respuesta JSON
-userSchema.methods.toJSON = function() {
-    var obj = this.toObject();
-    delete obj.password;
-    return obj;  }
+userSchema.methods.toJSON = function () {
+  var obj = this.toObject();
+  delete obj.password;
+  return obj;
+}
 
 // Convertir a modelo
 const User = mongoose.model('User', userSchema);
