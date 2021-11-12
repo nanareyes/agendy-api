fdc45a5e4b95a3a76a3bcea541c6ee62f5107ff3
nanareyes <dianac.reyesmantilla@gmail.com>
2021-11-12 09:00:29 -0500

    Se incluye en el schema los campos de googleid, registerSource y una condicion para el password para identificar si es o no requerido en el registro

--- models/user.js
+++ models/user.js
@@ -2,19 +2,26 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  id: { type: String},
+  id: { type: String },
+  googleId: { type: String },
+  registerSource: { type: String },
   identification: { type: Number },
   name: { type: String, required: [true, 'Nombre obligatorio'] },
-  lastName: { type: String},
+  lastName: { type: String },
   dateOfBirth: { type: Date },
   address: { type: String },
   city: { type: String },
-  department: { type: String},
+  department: { type: String },
   phone: { type: Number },
   email: { type: String, required: [true, 'Email obligatorio'] },
-  password: { type: String, required: [true, 'Contraseña obligatoria'] },
-  image: {type: String },
-  userType: { type: String},
+  password: {
+    type: String,
+    required: function () {
+      return [this.registerSource === 'AGENDY', 'Contraseña obligatoria']
+    }
+  },
+  image: { type: String },
+  userType: { type: String },
   terms: { type: Boolean },
 });
 
